b7dea82475baf51ab1ed926ded1181f4a6e99a88
Shree <shree912@yahoo.com>
2023-06-24 14:02:43 -0700

    model test

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
-
+require 'byebug'
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {User.new(id:1,username: "Jack", name: "Smith", email: "jsmith@sample.com" )}
+  subject { Tweet.new(id:11,content: "new tweet conten", user_id: user.id,user:user )}
+  
+  it "is not valid without a content" do
+    subject.content=nil
+    expect(subject).to_not be_valid
+  end
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+  it "is not valid without a user" do
+    subject.user_id=nil
+    expect(subject).to_not be_valid
+  end
 end
